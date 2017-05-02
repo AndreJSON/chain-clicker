12b7d607f4a4a24f0cfe1873851d18224c754095
AndreJSON <andre.v.json@gmail.com>
2017-05-02 14:09:44 +0200

    Adds ability to create and run scripts.

--- Clicker.java
+++ Clicker.java
@@ -1,17 +1,34 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.io.*;
 
 public class Clicker {
+	static final int NORMAL = 1, STORE = 2, LOAD = 3;
 	Robot robot;
 	Scanner scan;
 
 	public Clicker() throws Exception {
-		robot = new Robot();
-		robot.setAutoWaitForIdle(true);
-		scan = new Scanner(System.in);
-		ArrayList<InputNode> chain = readChain();
-		iterateChain(chain);
+		init();
+		ArrayList<InputNode> chain;
+		String name;
+		int mode = getMode();
+		switch(mode) {
+			case NORMAL:	chain = createChain();
+							iterateChain(chain);
+							break;
+			case STORE:		System.out.println("Name the chain:");
+							name = scan.nextLine();
+							chain = createChain();
+							storeChain(chain, name+".chain");
+							break;
+			case LOAD:		System.out.println("Name the chain:");
+							name = scan.nextLine();
+							chain = loadChain(name+".chain");
+							iterateChain(chain);
+							break;
+			default: System.exit(-1);
+		}
 	}
 
 	private void iterateChain(ArrayList<InputNode> chain) throws Exception {
@@ -22,7 +39,7 @@ public class Clicker {
 		}
 	}
 
-	private ArrayList<InputNode> readChain() {
+	private ArrayList<InputNode> createChain() {
 		System.out.println("How many nodes should the chain have?");
 		ArrayList<InputNode> chain = new ArrayList<>();
 		int input, chainLength = scan.nextInt();
@@ -43,7 +60,42 @@ public class Clicker {
 		robot.mouseMove((int)p.getX(),(int)p.getY());
 		robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
 		robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-		robot.delay(wait);
+		Thread.sleep(wait);
+	}
+
+	private void storeChain(ArrayList<InputNode> chain, String name) throws Exception {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(name));
+		for(InputNode node : chain) {
+			writer.write(node.toString() + "\n");
+		}
+		writer.close();
+	}
+
+	private ArrayList<InputNode> loadChain(String name) throws Exception {
+		ArrayList<InputNode> chain = new ArrayList<>();
+		BufferedReader reader = new BufferedReader(new FileReader(name));
+		String line;
+		while((line = reader.readLine()) != null) {
+			chain.add(new InputNode(line));
+		}
+		return chain;
+	}
+
+	private int getMode() {
+		System.out.println("########################");
+		System.out.println("#   [1] Normal mode    #");
+		System.out.println("#   [2] Create chain   #");
+		System.out.println("#   [3] Load chain     #");
+		System.out.println("########################");
+		int mode = scan.nextInt();
+		scan.nextLine(); //Consume the trailing newline from nextInt()
+		return mode;
+	}
+
+	private void init() throws Exception {
+		robot = new Robot();
+		robot.setAutoWaitForIdle(true);
+		scan = new Scanner(System.in);
 	}
 
 	public static void main(String[] args) {
@@ -52,5 +104,5 @@ public class Clicker {
 		} catch(Exception e) {
 			System.out.println(e);
 		}
-	}
+	}	
 }
--- InputNode.java
+++ InputNode.java
@@ -9,11 +9,25 @@ public class InputNode {
 		this.millisToNext = millisToNext;
 	}
 
+	/**
+	 * Intatiate an InputNode from a string representation.
+	 * For more information on the format, view the toString method.
+	 */
+	public InputNode(String s) {
+		String[] params = s.split("\\s");
+		this.point = new Point(Integer.parseInt(params[0]), Integer.parseInt(params[1]));
+		this.millisToNext = Integer.parseInt(params[2]);
+	}
+
 	public int getMillisToNext() {
 		return millisToNext;
 	}
 
 	public Point getPoint() {
 		return point;
+	}
+
+	public String toString() {
+		return "" + (int)point.getX() + " " + (int)point.getY() + " " + millisToNext;
 	}
 }
